43e196423266ac342951d2bc34e5a4aaca4f9243
parth.khunteta <pkhunt@icims.com>
2021-05-31 08:02:03 +0530

    updtae the app to home page when user logged in or sign in

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import './App.css';
 import { connect } from 'react-redux';
 import HomePage from './pages/homepage/Homepage'
 import ShopPage from './pages/shop/shop'
 import Header from './components/Header/Header'
-import SignIn from './pages/sign-in-sign-up/sign-in-sign-up'
+import SignInSignUp from './pages/sign-in-sign-up/sign-in-sign-up'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions'
 
@@ -44,7 +44,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/shop' component={ShopPage} />
-          <Route exact path='/signin' component={SignIn} />
+          <Route exact path='/signin' render={() => this.props.currentuser ? (<Redirect to='/' />) : (<SignInSignUp />)} />
         </Switch>
       </div>
     );
@@ -52,7 +52,11 @@ class App extends React.Component {
 
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentuser: user.currentuser
+})
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
